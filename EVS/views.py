fb7719002b574f6ede2cde7f5afaa3a62d162405
Remwell B. Pepito <20220025023@my.xu.edu.ph>
2025-04-15 16:22:22 +0800

    changed burger into flowting

--- EVS/views.py
+++ EVS/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
-from .models import student
+from .models import Ticket
+from django.core.paginator import Paginator
 #import login_required
 
 # Create your views here.
 def dashboard_view(request):
-    students = student.objects.all()
     return render(request, 'system/dashboard.html')
 
 #@login_required (ALL FUNCTION)
 def violation_views(request):
-    return render(request, 'system/tickets.html')
+    tickets = Ticket.objects.all().order_by('-date_created')
+    paginator = Paginator(tickets, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'tickets': page_obj,
+    }
+    return render(request, 'system/tickets.html', context)
 
 def tally_views(request):
     return render(request, 'system/tally.html')
